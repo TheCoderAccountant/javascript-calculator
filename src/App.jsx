5256f0ec1300b6f081ac61aa353c772d7876c8c4
Nurlan Khammadli <nurlanxm@gmail.com>
2023-08-04 17:57:33 +0400

    input functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [result, setResult] = useState(0)
+  const [prevResult, setPrevResult] = useState(0)
+
+  function ResetAll() {
+    setResult(result => 0);
+    setPrevResult(prevResult => 0)
+  }
+
+  function HandleInput(event) {
+    const value = event.target.textContent;
+
+    setResult(result => +(result + value));
+  }
+
+  function Operation(event) {
+    setPrevResult(prevResult => result);
+    const value = event.target.textContent;
+    setResult(result => value);
+  }
+
 
   return (
     <>
       <div id="calculator">
         <div className="display-area">
-          <div id="display-calcs">1</div>
-          <div id="display">2</div>
+          <div id="display-calcs">{prevResult}</div>
+          <div id="display">{result}</div>
         </div>
         <div className='main-area'>
-          <button id="clear">AC</button>
-          <button id="divide">/</button>
-          <button id="multiply">X</button>
-          <button className="numbers" id="seven">7</button>
-          <button className="numbers" id="eight">8</button>
-          <button className="numbers" id="nine">9</button>
-          <button id="subtract">-</button>
-          <button className="numbers" id="four">4</button>
-          <button className="numbers" id="five">5</button>
-          <button className="numbers" id="six">6</button>
-          <button id="add">+</button>
-          <button className="numbers" id="one">1</button>
-          <button className="numbers" id="two">2</button>
-          <button className="numbers" id="three">3</button>
+          <button id="clear" onClick={ResetAll}>AC</button>
+          <button id="divide" onClick={Operation}>/</button>
+          <button id="multiply" onClick={Operation}>X</button>
+          <button className="numbers" id="seven" onClick={HandleInput}>7</button>
+          <button className="numbers" id="eight" onClick={HandleInput}>8</button>
+          <button className="numbers" id="nine" onClick={HandleInput}>9</button>
+          <button id="subtract" onClick={Operation}>-</button>
+          <button className="numbers" id="four" onClick={HandleInput}>4</button>
+          <button className="numbers" id="five" onClick={HandleInput}>5</button>
+          <button className="numbers" id="six" onClick={HandleInput}>6</button>
+          <button id="add" onClick={Operation}>+</button>
+          <button className="numbers" id="one" onClick={HandleInput}>1</button>
+          <button className="numbers" id="two" onClick={HandleInput}>2</button>
+          <button className="numbers" id="three" onClick={HandleInput}>3</button>
           <button id="equals">=</button>
-          <button className="numbers" id="zero">0</button>
+          <button className="numbers" id="zero" onClick={HandleInput}>0</button>
           <button className="numbers" id="decimal">.</button>
         </div>
       </div>
@@ -43,6 +62,6 @@ function App() {
 export default App
 
 
-{/* <button onClick={() => setCount((count) => count + 1)}>
-count is {count}
-</button> */}
+// {<button onClick={() => setCount((count) => count + 1)}>
+// count is {count}
+// </button> }
